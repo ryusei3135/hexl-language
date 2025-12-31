f063d8078a28b5ce3d5858812806bbd5e8400aab
ryusei3135 <ryusei0220main@gmail.com>
2025-12-31 22:41:08 +0900

    alpha 2 ver 0.1.10

--- src/runner/run.rs
+++ src/runner/run.rs
@@ -3,63 +3,31 @@ use crate::manager::global_state::func_manager;
 use crate::runner::storage::var;
 
 
+fn get_left_value(node: &node::CalculNode) -> i32 {
+    node_run(*node.left_node.clone().unwrap()).parse::<i32>().unwrap()
+}
+
+fn get_right_value(node: &node::CalculNode) -> i32 {
+    node_run(*node.right_node.clone().unwrap()).parse::<i32>().unwrap()
+}
+
 pub fn node_run(
         node: node::CalculNode,
 ) -> String {
     match node.node_type {
-        node::NodeKind::NodeNum => {
-            node.value.clone()
-        },
-        node::NodeKind::NodeNot => {
-            let left_value = node_run(*node.left_node.unwrap());
-            let result = !left_value.parse::<i32>().unwrap();
-            result.to_string()
-        }
-        node::NodeKind::NodeStr => {
-            node.value.clone()
-        }
-        node::NodeKind::NodeAdd => {
-            let left_value = node_run(*node.left_node.unwrap());
-            let right_value = node_run(*node.right_node.unwrap());
-            let result = left_value.parse::<i32>().unwrap() + right_value.parse::<i32>().unwrap();
-            result.to_string()
-        },
-        node::NodeKind::NodeSub => {
-            let left_value = node_run(*node.left_node.unwrap());
-            let right_value = node_run(*node.right_node.unwrap());
-            let result = left_value.parse::<i32>().unwrap() - right_value.parse::<i32>().unwrap();
-            result.to_string()
-        },
-        node::NodeKind::NodeMul => {
-            let left_value = node_run(*node.left_node.unwrap());
-            let right_value = node_run(*node.right_node.unwrap());
-            let result = left_value.parse::<i32>().unwrap() * right_value.parse::<i32>().unwrap();
-            result.to_string()
-        },
-        node::NodeKind::NodeDiv => {
-            let left_value = node_run(*node.left_node.unwrap());
-            let right_value = node_run(*node.right_node.unwrap());
-            let result = left_value.parse::<i32>().unwrap() / right_value.parse::<i32>().unwrap();
-            result.to_string()
-        },
-        node::NodeKind::NodeEqTo => {
-            let left_value = node_run(*node.left_node.unwrap());
-            let right_value = node_run(*node.right_node.unwrap());
-            let result = left_value.parse::<i32>().unwrap() == right_value.parse::<i32>().unwrap();
-            result.to_string()
-        },
-        node::NodeKind::NodeNotEqTo => {
-            let left_value = node_run(*node.left_node.unwrap());
-            let right_value = node_run(*node.right_node.unwrap());
-            let result = left_value.parse::<i32>().unwrap() != right_value.parse::<i32>().unwrap();
-            result.to_string()
-        }
-        node::NodeKind::NodeAssignVar => return var::update_var_value(node.clone()),
-        node::NodeKind::NodeCallVar => return var::call_var_value(node.value),
-        node::NodeKind::NodeDefVar => return var::define_var(node.clone()),
-        node::NodeKind::NodeCallFunc => {
-            return run_func(func_manager().get_func(&node.value.clone()));
-        },
+        node::NodeKind::NodeNum => node.value.clone(),
+        node::NodeKind::NodeNot => (!get_left_value(&node)).to_string(),
+        node::NodeKind::NodeStr => node.value.clone(),
+        node::NodeKind::NodeAdd => (get_left_value(&node) + get_right_value(&node)).to_string(),
+        node::NodeKind::NodeSub => (get_left_value(&node) - get_right_value(&node)).to_string(),
+        node::NodeKind::NodeMul => (get_left_value(&node) * get_right_value(&node)).to_string(),
+        node::NodeKind::NodeDiv => (get_left_value(&node) / get_right_value(&node)).to_string(),
+        node::NodeKind::NodeEqTo => (get_left_value(&node) == get_right_value(&node)).to_string(),
+        node::NodeKind::NodeNotEqTo => (get_left_value(&node) != get_right_value(&node)).to_string(),
+        node::NodeKind::NodeAssignVar => var::update_var_value(node.clone()),
+        node::NodeKind::NodeCallVar => var::call_var_value(node.value),
+        node::NodeKind::NodeDefVar => var::define_var(node.clone()),
+        node::NodeKind::NodeCallFunc => run_func(func_manager().get_func(&node.value.clone())),
         _ => {
             String::new()
         }
